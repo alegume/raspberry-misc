10968f3b2a8d6b390e0fcc21159d6af454b267f4
alegume <alexandreabreu@comp.ufla.br>
2019-06-04 16:24:17 -0300

    trabalhando com worksheets

--- temperatura-interna-raspberry.py
+++ temperatura-interna-raspberry.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import os
+import socket
 import time
 from datetime import datetime
 import csv
@@ -7,17 +8,21 @@ import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
+hostname = socket.gethostname()
 
-# use creds to create a client to interact with the Google Drive API
+# Credenciais do Google Drive API
 scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
 creds = ServiceAccountCredentials.from_json_keyfile_name(os.path.join(dir_path, 'secret_key.json'), scope)
 client = gspread.authorize(creds)
-sheet = client.open('temperatura-raspberry').sheet1
+# Abre uma o documeto (spreadsheet)
+spreadsheet = client.open('temperatura-raspberryS')
+# Abre a planilha (worksheet)
+worksheet = spreadsheet.worksheet(hostname)
 
-# Return CPU temperature as a character string
 def getCPUtemperature():
 	res = os.popen('vcgencmd measure_temp').readline()
 	return(res.replace('temp=', '').replace("'C\n", ''))
+
 
 while (True):
 	temperatura = float(getCPUtemperature())
@@ -26,7 +31,7 @@ while (True):
 	row = [datetime.now().strftime('%d/%m/%Y %H:%M:%-S'), temperatura]
 
 	try :
-		sheet.append_row(row)
+		worksheet.append_row(row)
 	except Exception as e:
 		print(e)
 		print('Erro ao enviar dados para a nuvem')
