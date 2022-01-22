7f237dc2f6cf7c227faba62d8b0e781fe8288b09
Alexandre Abreu <alexandregrandeabreu@gmail.com>
2022-01-22 15:58:43 -0300

    atualizacao do script

--- temperatura-interna-raspberry.py
+++ temperatura-interna-raspberry.py
@@ -5,30 +5,40 @@ import socket
 from datetime import datetime
 import csv
 import gspread
-from oauth2client.service_account import ServiceAccountCredentials
+# from oauth2client.service_account import ServiceAccountCredentials
 
-dir_path = os.path.dirname(os.path.realpath(__file__))
-hostname = socket.gethostname()
+# dir_path = os.path.dirname(os.path.realpath(__file__))
+# hostname = socket.gethostname()
+# # Credenciais do Google Drive API
+# scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
+# creds = ServiceAccountCredentials.from_json_keyfile_name(os.path.join(dir_path, 'secret_key.json'), scope)
+# client = gspread.authorize(creds)
+# # Abre uma o documeto (spreadsheet)
+# spreadsheet = client.open(hostname)
+# # Abre a planilha (worksheet)
+# worksheet = spreadsheet.worksheet('temperatura-cpu')
 
-# Credenciais do Google Drive API
-scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
-creds = ServiceAccountCredentials.from_json_keyfile_name(os.path.join(dir_path, 'secret_key.json'), scope)
-client = gspread.authorize(creds)
-# Abre uma o documeto (spreadsheet)
-spreadsheet = client.open(hostname)
-# Abre a planilha (worksheet)
-worksheet = spreadsheet.worksheet('temperatura-cpu')
+def conectar():
+    # Conectar
+    gc = gspread.service_account()
+    # Encontrar planilha
+    sh = gc.open("arquitetura-responsiva")
+    # Encontrar primeira aba
+    ws = sh.worksheets()[0]
+    return ws
 
-def getCPUtemperature():
+
+
+def temperatura():
 	res = os.popen('vcgencmd measure_temp').readline()
 	return(res.replace('temp=', '').replace("'C\n", ''))
 
 
-temperatura = float(getCPUtemperature())
 
+temperatura = float(temperatura())
 # data : hora, temperatura
 row = [datetime.now().strftime('%d/%m/%Y %H:%M:%-S'), temperatura]
-
+worksheet = conectar()
 try :
 	worksheet.append_row(row)
 except Exception as e:
